5894cb333965758b8679dcc5c60465414a107091
Piyush Varshney <piyushvarshney54@gmail.com>
2023-09-13 21:46:30 +0530

    adding new work

---
+++ arraynew4.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="this is string ";
+    int l = 0;
+    int i = 0;
+    while(s[i] != '\0')
+    {
+        i++;
+        l++;
+    }
+    cout<<l;
+    return 0;
+}
