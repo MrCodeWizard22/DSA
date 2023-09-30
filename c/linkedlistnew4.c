b8cf3a088b2ce45f19fcdcc83e01dd05cc165f5d
Piyush Varshney <piyushvarshney54@gmail.com>
2023-09-30 11:59:26 +0530

    new files added

---
+++ c/linkedlistnew4.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+};
+void inserttail(struct node ** head, int data){
+    struct node *newnode =(struct node *)malloc (sizeof(struct node ));
+    struct node *temp=*head;
+    newnode ->data=data;
+    newnode ->next=NULL;
+    if(*head==0)
+        *head =newnode;
+    
+    else {
+        while(temp->next!=0){
+            temp=temp->next;
+        }
+        temp->next =newnode;
+    }
+}
+void print (struct node ** head){
+    struct node *ptr =*head;
+    while(ptr->next!=0){
+        printf("%d-->",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("%d",ptr->data);
+}
+void deletehead(struct node **head){
+    struct node *temp=*head;
+    *head =temp->next;
+    temp->next =NULL;
+    // free(temp);
+}
+void deletetail(struct node ** head){
+    struct node *temp=*head;
+    struct node *prev=*head;
+    while(temp->next !=NULL){
+        prev=temp;
+        temp=temp->next;
+    }
+    prev->next=NULL;
+    // free(prev);
+}
+void deletepos(struct node** head ,int pos){
+    int index=0;
+    struct node *temp=*head;
+    struct node *prev=*head;
+    while(temp->next !=0 && index<pos){
+        prev=temp;
+        temp=temp->next;
+        index++;
+
+    }
+    prev->next =temp->next;
+    temp->next=NULL;
+}
+int main()
+{
+    struct node *head=0;
+    inserttail(&head ,10);
+    inserttail(&head ,7);
+    inserttail(&head ,49);
+    inserttail(&head, 18);
+    inserttail(&head ,45);
+    // deletehead(&head);
+    // deletetail(&head);
+    deletepos(&head ,3);
+    // deletehead(&head);
+
+
+    print (&head);
+    return 0;
+}
