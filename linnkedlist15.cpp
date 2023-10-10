74526e3c885b4a91a87ac03e87c06097f640143b
Piyush Varshney <piyushvarshney54@gmail.com>
2023-10-10 18:53:25 +0530

    learned insertion in circular linked list

--- linnkedlist15.cpp
+++ linnkedlist15.cpp
@@ -6,7 +6,7 @@ struct node {
     struct node *next ;
 
 };
-void create(struct node ** head, int data){
+void create(struct node ** head, struct node **tail,int data){
     struct node *newnode =(struct node *)malloc (sizeof(struct node) );
     struct node *temp=*head;
 
@@ -14,13 +14,61 @@ void create(struct node ** head, int data){
     if(*head==NULL){
         *head=newnode ;
         temp=newnode ;
+        *tail=newnode ;
     }
     else {
         temp->next=newnode ;
         temp=newnode ;
 
     }
+    *tail=newnode ;
     temp->next =*head;
+
+}
+
+// insertion at head
+void inserthead(struct node ** head, struct node **tail, int data){
+    struct node *newnode =(struct node *)malloc (sizeof(struct node ));
+    newnode ->data=data;
+
+    newnode ->next =*head;
+    *head=newnode ;
+    (*tail)->next =newnode ;
+
+}
+// insertion at tail 
+
+void insertend(struct node **tail, int data){
+    struct node *newnode=(struct node *)malloc (sizeof(struct node ));
+    struct node *tamp=*tail;
+
+    newnode ->data=data;
+
+    newnode ->next =(*tail)->next ;
+    (*tail)->next =newnode ;
+
+    *tail=newnode ;
+
+
+
+}
+
+// insertion at position 
+
+void insertpos(struct node **head, int data , int pos){
+    struct node *newnode =(struct node *)malloc (sizeof(struct node));
+    struct node *temp=*head;
+    int count=1;
+
+    newnode ->data=data;
+    while(temp->next !=*head && count <pos-1){
+        temp=temp->next;
+        count++;
+    }
+    newnode ->next =temp->next;
+    temp->next =newnode ;
+
+
 
 }
 void display(struct node ** head){
@@ -30,16 +78,27 @@ void display(struct node ** head){
         temp=temp->next ;
 
     }
-    cout<<temp->data<<;
+    cout<<temp->data;
     // temp=temp->next ;
     // cout<<temp->data;
 }
 int main(){
     struct node *head=NULL;
+    struct node *tail=NULL;
 
-    create(&head, 7);
+    create(&head,&tail, 7);
 
-    create(&head, 5);
+    create(&head,&tail, 5);
+
+    inserthead(&head, &tail, 6);
+
+    inserthead(&head , &tail, 8);
+
+    insertend(&tail, 18);
+
+    insertpos(&head, 102, 2);
+
+    insertpos(&head, 105, 3);
     display(&head);
     return 0;
 }
